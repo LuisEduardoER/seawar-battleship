64107868ecdc058716c15626c3ae96686bfc4e82
cody.br@gmail.com <cody.br@gmail.com@d690e34a-18bc-ee2b-21b0-ff2b94e33d8f>
2011-05-21 02:47:55 +0000

    Criadas as implementações iniciais das classes Jogador e Usuário , também os stubs de seus respectivos DAO's :) 15/05 <- para adicionar na ATA se for preciso

---
+++ trunk/seawar/src/JogadorDAO.java
@@ -0,0 +1,28 @@
+
+public class JogadorDAO {
+
+	public static boolean IsOnline(int IdUsuario) {
+		// TODO Implementar para verificar no banco de dados
+		return false;
+	}
+
+	public static boolean IsJogando(int IdUsuario) {
+		// TODO Implementar para verificar no banco de dados 
+		//se tal jogador está jogando :)
+		
+		return true;
+	}
+
+	public static boolean InserirJogador(Jogador jogador) {
+		// TODO Implementar para inserir o jogador no banco de dados
+		//Esta ação é semelhante a "desconectar jogador"
+		return false;
+	}
+	
+	public static boolean RemoverJogador(Jogador jogador) {
+		// TODO Implementar para remover o jogador no banco de dados
+		//Esta ação é semelhante a "desconectar jogador"
+		return false;
+	}
+
+}
