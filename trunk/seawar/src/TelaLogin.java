70d2b303bd9ebc1d7f24fd159dfcff3cd985b97e
f.sevla@ig.com.br <f.sevla@ig.com.br@d690e34a-18bc-ee2b-21b0-ff2b94e33d8f>
2011-05-21 17:30:35 +0000

    Login, inicio da tela para selecionar jogador

--- trunk/seawar/src/TelaLogin.java
+++ trunk/seawar/src/TelaLogin.java
@@ -14,30 +14,26 @@
       private JButton cancelar;  
      
        public void init()
-      {
-         
+      {       
          setLayout(new FlowLayout());
-      
+         setSize(200,80);
          labUsuario = new JLabel("Usuário");
          textUsuario = new JTextField("",11);
          labSenha = new JLabel("Senha   ");
          senha = new JPasswordField(11);
-         ok = new JButton("OK");
-         ok.setMnemonic( 'O' );
+         ok = new JButton("Entrar");
+         ok.setMnemonic( 'E' );
       	//para desabilitar um JTextField textUsuario.setEditable(false);
          	
          cancelar = new JButton("Cancelar");
          cancelar.setMnemonic( 'C' );
-         
-      						
+               						
          add(labUsuario);
          add(textUsuario);
          add(labSenha);
          add(senha);
          add(ok);
-         add(cancelar);
-      	
-      }
-   	
-   	
+         add(cancelar);      	
+      }   	   	
    }
+    
