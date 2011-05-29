e3c303d2de526c49e598d2ca562be410f3f8c2b2
marcusviniciuspalota@hotmail.com <marcusviniciuspalota@hotmail.com@d690e34a-18bc-ee2b-21b0-ff2b94e33d8f>
2011-05-29 04:47:59 +0000

    Criado os métodos da classe JogadorDAO

--- trunk/seawar/src/JogadorDAO.java
+++ trunk/seawar/src/JogadorDAO.java
@@ -1,28 +1,120 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class JogadorDAO {
 
 	public static boolean IsOnline(int IdUsuario) {
 		// TODO Implementar para verificar no banco de dados
-		return false;
+		boolean bIsOnline = false;
+		BancoDeDados objBD = new BancoDeDados();
+		if(objBD.abrirConexao())
+		{
+			try
+			{
+				//Foi utilizado a classe StringBuilder, que nesse caso realiza a mesma função do que uma
+				//concatenação de strings
+				StringBuilder strSQL = new StringBuilder("SELECT STATUS_USUARIO FROM SEAWAR.USUARIO");
+				strSQL.append(" WHERE COD_ID_USUARIO = " + IdUsuario);
+				ResultSet objRS = objBD.getObjStatement().executeQuery(strSQL.toString());
+				objRS.first();
+				bIsOnline =  objRS.getBoolean("STATUS_USUARIO");
+				objRS.close();
+				objBD.fecharConexaoBanco(objBD.getObjConn());
+			}
+			catch (SQLException ex)
+			{
+				Logger.getLogger(Jogador.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+		return bIsOnline;
 	}
 
 	public static boolean IsJogando(int IdUsuario) {
 		// TODO Implementar para verificar no banco de dados 
 		//se tal jogador est� jogando :)
-		
-		return true;
+		boolean bIsJogando = false;
+		BancoDeDados objBD = new BancoDeDados();
+		if(objBD.abrirConexao())
+		{
+			try
+			{
+				//Foi utilizado a classe StringBuilder, que nesse caso realiza a mesma função do que uma
+				//concatenação de strings
+				StringBuilder strSQL = new StringBuilder("SELECT STATUS_JOGADOR FROM SEAWAR.JOGADOR");
+				strSQL.append(" WHERE COD_ID_USUARIO = " + IdUsuario);
+				ResultSet objRS = objBD.getObjStatement().executeQuery(strSQL.toString());
+				objRS.first();
+				bIsJogando =  objRS.getBoolean("STATUS_JOGADOR");
+				objRS.close();
+				objBD.fecharConexaoBanco(objBD.getObjConn());
+			}
+			catch (SQLException ex)
+			{
+				Logger.getLogger(Jogador.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+		return bIsJogando;
 	}
 
 	public static boolean InserirJogador(Jogador jogador) {
 		// TODO Implementar para inserir o jogador no banco de dados
-		//Esta a��o � semelhante a "desconectar jogador"
-		return false;
+		BancoDeDados objBD = new BancoDeDados();
+		if(objBD.abrirConexao())
+		{
+			try
+			{
+				//Foi utilizado a classe StringBuilder, que nesse caso realiza a mesma função do que uma
+				//concatenação de strings
+				StringBuilder strSQL = new StringBuilder("INSERT INTO SEAWAR.JOGADOR(COD_ID_USUARIO, STATUS_JOGADOR, IP_JOGADOR) VALUES (");
+				strSQL.append(jogador.getId_usuario() +"," );
+				strSQL.append(jogador.isJogando() +"," );
+				strSQL.append("'"+jogador.getIpJogador()+"'");
+				strSQL.append(");");
+
+				if(objBD.getObjStatement().execute(strSQL.toString()))
+				{
+					return true;
+				}
+				objBD.fecharConexaoBanco(objBD.getObjConn());
+			}
+			catch (SQLException ex)
+			{
+				Logger.getLogger(Jogador.class.getName()).log(Level.SEVERE, null, ex);
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	public static boolean RemoverJogador(Jogador jogador) {
 		// TODO Implementar para remover o jogador no banco de dados
 		//Esta a��o � semelhante a "desconectar jogador"
-		return false;
+		BancoDeDados objBD = new BancoDeDados();
+		if(objBD.abrirConexao())
+		{
+			try
+			{
+				//Foi utilizado a classe StringBuilder, que nesse caso realiza a mesma função do que uma
+				//concatenação de strings
+				StringBuilder strSQL = new StringBuilder("DELETE FROM SEAWAR.JOGADOR");
+				strSQL.append(" WHERE COD_ID_USUARIO = " + jogador.getId_usuario());
+
+				if(objBD.getObjStatement().execute(strSQL.toString()))
+				{
+					return true;
+				}
+				objBD.fecharConexaoBanco(objBD.getObjConn());
+			}
+			catch (SQLException ex)
+			{
+				Logger.getLogger(Jogador.class.getName()).log(Level.SEVERE, null, ex);
+				return false;
+			}
+		}
+		return true; 
 	}
 
 }
