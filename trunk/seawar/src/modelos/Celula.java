2228978d6bce6d63c43b2bcf3f71bc2a009cd5d9
cody.br@gmail.com <cody.br@gmail.com@d690e34a-18bc-ee2b-21b0-ff2b94e33d8f>
2011-09-04 22:46:35 +0000

    Flag se a celula foi acertada ou nao

--- trunk/seawar/src/modelos/Celula.java
+++ trunk/seawar/src/modelos/Celula.java
@@ -21,9 +21,11 @@ public class Celula extends Point implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	boolean acertada;
 	
 	public Celula(){
 		aTipoCelula = TipoCelula.AreaLivre;
+		acertada = false;
 	}
 	
 	public TipoCelula aTipoCelula;
@@ -35,7 +37,12 @@ public class Celula extends Point implements Serializable{
 	public void setTipoCelula(TipoCelula tipoCelula) {
 		aTipoCelula = tipoCelula;
 	}
-	
+	public void acertar(){
+		acertada = true;
+	}
+	public boolean isAtirada(){
+		return acertada;
+	}
 	public void carregarImagem(TipoCelula tipoCelula) {
 		//TODO: Rever esta implementação, verificar na doc.
 	}
